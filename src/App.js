701df0baf5df3a6397bd7f79e3b82bb8b920bc77
serenitysmith <serenitysmith24@gmail.com>
2023-09-30 17:55:26 -0400

    inputs with forms react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+import "./App.css"
+
+import React, {useState} from "react"
+
+export default function App() {
+  const [firstName, setFirstName] = React.useState("")
+  
+
+
+  console.log(firstName)
+  function handleChange(event) {
+    // console logs the jsx element targeted IE logs <input type="text" placeholder="First Name"> when we add value to it, it adds what was enteredd in the input box 
+      // console.log(event.target.value)
+
+      // this changes the state every time somehtign is entered in the input box and console logs its 
+
+      setFirstName(event.target.value)
+
+
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <form>
+          <input
+              type="text"
+              placeholder="First Name"
+              onChange={handleChange}
+          />
+      </form>
+  )
 }
 
-export default App;
