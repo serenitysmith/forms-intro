b93fed9e5c0d0dfe31e5cc7b21d65c6c991ff315
serenitysmith <serenitysmith24@gmail.com>
2023-10-01 15:32:12 -0400

    notes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 ///
-import './App.css';
+
 
 import "./App.css"
 
@@ -15,7 +15,7 @@ export default function App() {
     // console logs the jsx element targeted IE logs <input type="text" placeholder="First Name"> when we add value to it, it adds what was enteredd in the input box 
       // console.log(event.target.value)
 
-      // this changes the state every time somehtign is entered in the input box and console logs its 
+      // this changes the state every time something is entered in the input box and console logs its 
 
       setFirstName(event.target.value)
 
